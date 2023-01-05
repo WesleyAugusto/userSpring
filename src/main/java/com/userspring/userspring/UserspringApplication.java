9a251474decc6c83ab10e32e92b730e13452202d
WesleyAugusto <ranzerap@gmail.com>
2023-01-05 19:46:48 -0300

    java spring

---
+++ src/main/java/com/userspring/userspring/UserspringApplication.java
@@ -0,0 +1,13 @@
+package com.userspring.userspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserspringApplication.class, args);
+	}
+
+}
